c6f0410dbb5fe42077c69637e2c2f55804cc892a
BULENS Virgil <github@anonymousdaddy.anonaddy.com>
2021-11-04 15:40:21 +0100

    updated variables.ps1 to add functions to export

--- VirgilsPSToolkit/build/variables.ps1
+++ VirgilsPSToolkit/build/variables.ps1
@@ -2,10 +2,20 @@ $ManifestPath = "$PSScriptRoot\..\VirgilsPSToolkit.psd1"
 
 # Update version in manifest file
 $ManifestContent = Get-Content -Path $ManifestPath -Raw 
-$ManifestContent.Replace("<ModuleVersion>", "1." + $env:GITHUB_RUN_NUMBER)
+$ManifestContent = $ManifestContent.Replace("<ModuleVersion>", "1." + $env:GITHUB_RUN_NUMBER)
 
 
 # Update functions to export in manifest file
+$FunctionsFolderPath = "$PSScriptRoot\..\functions"
+if ((Test-Path -Path $FunctionsFolderPath) -and ($FunctionNames = Get-ChildItem -Path $FunctionsFolderPath -Filter '*.ps1' | Select-Object -ExpandProperty BaseName))
+{
+    $FunctionStrings = "'$($FunctionNames -join "','")'"
+}
+else
+{
+    $FunctionStrings = $null
+}
+$ManifestContent = $ManifestContent.Replace("<FunctionsToExport>", $FunctionStrings)
 
 # Set new content of manifest file
 Set-Content -Path $ManifestPath -Value $ManifestContent -Force
