30d0461513a84f1cbb61620a200053ef8eacf7ba
BULENS Virgil <github@anonymousdaddy.anonaddy.com>
2021-11-30 12:00:08 +0100

    added New-PSRunBookWebhook.ps1

---
+++ VirgilsPSToolkit/functions/New-PSRunBookWebhook.ps1
@@ -0,0 +1,21 @@
+#
+# Create webhook runbook from existing runbook
+#
+function New-PSRunBookWebhook {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
