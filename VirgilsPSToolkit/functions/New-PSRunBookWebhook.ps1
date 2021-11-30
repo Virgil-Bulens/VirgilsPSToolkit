8d920e67a3d153f31511df4d4aa77500469a3783
BULENS Virgil <github@anonymousdaddy.anonaddy.com>
2021-11-30 13:37:05 +0100

    added parameters

--- VirgilsPSToolkit/functions/New-PSRunBookWebhook.ps1
+++ VirgilsPSToolkit/functions/New-PSRunBookWebhook.ps1
@@ -4,11 +4,18 @@
 function New-PSRunBookWebhook {
     [CmdletBinding()]
     param (
-        
+        # Path to the runbook to create webhook from
+        [Parameter(
+            Mandatory = $true,
+            Position = 0
+        )]
+        [string]
+        $Runbook
     )
     
     begin {
-        
+        $ErrorActionPreference = "Stop"
+        $RunbookFile = Get-Item -Path $Runbook
     }
     
     process {
